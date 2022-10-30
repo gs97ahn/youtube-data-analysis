1b98b5dfce683c7a2447c1978d925c2366059521
gs97ahn <gs97ahn@naver.com>
2022-10-30 23:27:16 +0900

    [FEAT] logging add

--- utils/data_format.py
+++ utils/data_format.py
@@ -5,15 +5,15 @@ class DataFormat:
     def csv_saver(self, file_name_with_path, header, data):
         data_df = pd.DataFrame(data=data)
         data_df.to_csv(file_name_with_path, header=header, index=False)
-        print('CSV FILE SAVED:', file_name_with_path)
+        print('\nCSV FILE SAVED:', file_name_with_path)
 
     def csv_reader(self, file_name_with_path):
-        print('CSV FILE READ:', file_name_with_path)
+        print('\nCSV FILE READ:', file_name_with_path)
         return pd.read_csv(file_name_with_path)
 
     def txt_saver(self, file_name_with_path, data):
         with open(file_name_with_path, 'w') as f:
             f.write(''.join(data))
         f.close()
-        print('TXT FILE SAVED:', file_name_with_path)
+        print('\nTXT FILE SAVED:', file_name_with_path)
 
