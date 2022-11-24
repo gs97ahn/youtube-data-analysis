1b4b0117f778fa38d12cf83c8b9c6076d0ddb1e9
gs97ahn <gs97ahn@naver.com>
2022-11-24 23:28:04 +0900

    [FEAT] ratio and z-score calculation add and code clean up done

--- preprocess.py
+++ preprocess.py
@@ -1,111 +1,189 @@
 from config.config import Config
 from utils.data_formatter import DataFormatter
-from datetime import datetime
 from utils.data_preprocessor import DataPreprocessor
+from tqdm import tqdm
+from datetime import datetime
 
 import os
 import nltk
 import pandas as pd
 import datetime
+import time
 
-nltk.download('all')
+# nltk.download('all')
 config = Config()
-data_format = DataFormatter()
+data_formatter = DataFormatter()
 data_preprocessor = DataPreprocessor()
 
 
 def get_comment_data():
-    comment_data = dict()
-    comment_csv = dict()
-    comment_filename = dict()
-    date_relevant_comment_data = dict()
-    for status in config.status:
-        comment_data[status] = dict()
-        comment_csv[status] = dict()
-        date_relevant_comment_data[status] = dict()
-        comment_filename[status] = os.listdir(config.comments_csv_folder_path[status])
-        for category in config.categories:
-            print(status.upper(), category.upper())
-            comment_csv[status][category] = pd.DataFrame()
-            comment_data[status][category] = list()
-            date_relevant_comment_data[status][category] = list()
-            for file in comment_filename[status]:
-                if file.startswith(category):
-                    comment_csv[status][category] = pd.concat([
-                        comment_csv[status][category],
-                        data_format.csv_reader(
-                            os.path.join(config.comments_csv_folder_path[status], file)
-                         )
+    filenames = dict()
+    comments = dict()
+    data_a, data_1w, data_2w = dict(), dict(), dict()
+    for s in config.status:
+        filenames[s] = os.listdir(config.comments_csv_folder_path[s])
+        comments[s] = dict()
+        data_a[s], data_1w[s], data_2w[s] = dict(), dict(), dict()
+        for c in config.categories:
+            print('\n\n**********', s.upper(), c.upper(), '**********')
+            comments[s][c] = pd.DataFrame()
+            data_a[s][c], data_1w[s][c], data_2w[s][c] = list(), list(), list()
+            for f in filenames[s]:
+                if f.startswith(c):
+                    comments[s][c] = pd.concat([
+                        comments[s][c], data_formatter.csv_reader(os.path.join(config.comments_csv_folder_path[s], f))
                     ])
-                    date = datetime.date(int(file.split('category_')[1].split('-')[0]),
-                                         int(file.split('category_')[1].split('-')[1]),
-                                         int(file.split('category_')[1].split('-')[2].strip('.csv')))
-                    for row in data_format.csv_reader(
-                            os.path.join(config.comments_csv_folder_path[status], file)
-                    ).iterrows():
+                    date = get_date_from_filename(f, '.csv')
+                    null_error_cnt = 0
+                    for r in data_formatter.csv_reader(os.path.join(config.comments_csv_folder_path[s], f)).iterrows():
                         try:
-                            comment_date = datetime.date(int(row[1]['updated at'].split('-')[0]),
-                                                         int(row[1]['updated at'].split('-')[1]),
-                                                         int(row[1]['updated at'].split('-')[2].split('T')[0]))
-                        except AttributeError as e:
-                            print('ERROR: Updated date is null')
+                            comment_date = get_date_from_comment(r, 'T')
+                        except AttributeError:
+                            null_error_cnt += 1
                             continue
                         if date - comment_date < datetime.timedelta(weeks=1):
-                            date_relevant_comment_data[status][category].append(row[1]['comment text'])
-            comment_data[status][category].extend(comment_csv[status][category][config.comments_header[2]].tolist())
-    return comment_data, date_relevant_comment_data
+                            data_1w[s][c].append(r[1]['comment text'])
+                        elif datetime.timedelta(weeks=1) <= date - comment_date < datetime.timedelta(weeks=2):
+                            data_2w[s][c].append(r[1]['comment text'])
+                    if null_error_cnt != 0:
+                        print('ERROR: Found', null_error_cnt, 'null updated date')
+            data_a[s][c].extend(comments[s][c][config.comments_header[2]].tolist())
+    return [data_a, data_1w, data_2w]
 
 
-def preprocess_comment_data(comment_data, csv_folder_path):
-    tokenized_comments = dict()
-    punctuation_removed_comments = dict()
-    keep_english_only_comments = dict()
-    stopwords_removed_comments = dict()
-    word_stem_comments = dict()
-    word_count_comments = dict()
-    for status in config.status:
-        tokenized_comments[status] = dict()
-        punctuation_removed_comments[status] = dict()
-        keep_english_only_comments[status] = dict()
-        stopwords_removed_comments[status] = dict()
-        word_stem_comments[status] = dict()
-        word_count_comments[status] = dict()
-        for category in config.categories:
-            print('\n', status.upper(), category.upper(), '\n')
-            tokenized_comments[status][category] = DataPreprocessor.tokenize(comment_data[status][category])
-            punctuation_removed_comments[status][category] = DataPreprocessor.punctuation(
-                tokenized_comments[status][category]
-            )
-            keep_english_only_comments[status][category] = DataPreprocessor.english(
-                punctuation_removed_comments[status][category]
-            )
-            stopwords_removed_comments[status][category] = DataPreprocessor.stopwords(
-                keep_english_only_comments[status][category]
-            )
-            word_stem_comments[status][category] = DataPreprocessor.word_stem(
-                stopwords_removed_comments[status][category]
-            )
-            word_count_comments[status][category] = DataPreprocessor.word_count(word_stem_comments[status][category])
+def get_date_from_filename(file, split_type):
+    return datetime.date(int(file.split('category_')[1].split('-')[0]),
+                         int(file.split('category_')[1].split('-')[1]),
+                         int(file.split('category_')[1].split('-')[2].strip(split_type)))
 
-    for status in config.status:
-        if not os.path.isdir(csv_folder_path[status]):
-            os.makedirs(csv_folder_path[status])
-        for category in config.categories:
-            data_list = []
-            for key, value in word_count_comments[status][category].items():
-                data_list.append([key, value])
-            data_format.csv_saver(
-                os.path.join(
-                    csv_folder_path[status],
-                    config.csv_file_name[status][category].split(datetime.date.today().strftime('_%Y'))[0] + '.csv'
-                ),
-                config.comment_words_header,
-                data_list
+
+def get_date_from_comment(comment, split_type):
+    return datetime.date(int(comment[1]['updated at'].split('-')[0]),
+                         int(comment[1]['updated at'].split('-')[1]),
+                         int(comment[1]['updated at'].split('-')[2].split(split_type)[0]))
+
+
+def count(comments, token_function_option):
+    data, result = dict(), dict()
+    for s in config.status:
+        data[s], result[s] = dict(), dict()
+        for c in config.categories:
+            print('\n\n**********', s.upper(), c.upper(), '**********')
+            data[s][c] = count_cycle(comments[s][c], token_function_option)
+            result[s][c] = dict_to_2d_list(data[s][c])
+    return result
+
+
+def dict_to_2d_list(dict_data):
+    result = list()
+    for key, value in dict_data.items():
+        result.append([key, value])
+    result.sort(key=lambda x: x[1], reverse=True)
+    return result
+
+
+def count_cycle(data, token_function_option):
+    data = data_preprocessor.tokenize(data, token_function_option)
+    data = data_preprocessor.punctuation(data)
+    data = data_preprocessor.english(data)
+    data = data_preprocessor.stopwords(data)
+    data = data_preprocessor.word_stem(data)
+    data = data_preprocessor.word_count(data)
+    return data
+
+
+def ratio(counts, data_len):
+    r_data, result = dict(), dict()
+    for s in config.status:
+        r_data[s], result[s] = dict(), dict()
+        for c in config.categories:
+            print('\n\n**********', s.upper(), c.upper(), '**********')
+            if s == config.status[0]:
+                r_data[s][c] = data_preprocessor.calculate_ratio(counts, data_len, c)
+            elif s == config.status[1]:
+                r_data[s][c] = flip_sign(r_data[config.status[0]][c])
+            result[s][c] = dict_to_2d_list(r_data[s][c])
+    return result
+
+
+def flip_sign(dict_data):
+    time.sleep(0.5)
+    f_data = dict()
+    for key, value in tqdm(dict_data.items()):
+        f_data[key] = -value
+    return f_data
+
+
+def z_score(ratios):
+    result = dict()
+    for s in config.status:
+        result[s] = dict()
+        for c in config.categories:
+            print('\n\n**********', s.upper(), c.upper(), '**********')
+            result[s][c] = data_preprocessor.calculate_z_score(ratios[s][c])
+    return result
+
+
+def data_length(comment):
+    data_len = dict()
+    for s in config.status:
+        data_len[s] = dict()
+        for c in config.categories:
+            data_len[s][c] = len(comment)
+    return data_len
+
+
+def save_data_file(data, csv_folder_path, header_option):
+    header = None
+    if header_option == config.pdata_type[1][0]:
+        header = config.pdata_header[config.pdata_type[1][0]]
+    elif header_option == config.pdata_type[1][1]:
+        header = config.pdata_header[config.pdata_type[1][1]]
+    elif header_option == config.pdata_type[1][2]:
+        header = config.pdata_header[config.pdata_type[1][2]]
+    else:
+        print('ERROR: Unknown header option -', header_option)
+        exit(1)
+    for s in config.status:
+        if not os.path.isdir(csv_folder_path[s]):
+            os.makedirs(csv_folder_path[s])
+        for c in config.categories:
+            data_formatter.csv_saver(
+                os.path.join(csv_folder_path[s],
+                             config.csv_filename[s][c].split(datetime.date.today().strftime('_%Y'))[0] + '.csv'),
+                header,
+                data[s][c]
             )
 
 
 if __name__ == '__main__':
-    youtube_comment_data, date_relevant_youtube_comment_data = get_comment_data()
-    preprocess_comment_data(youtube_comment_data, config.comment_words_csv_folder_path)
-    preprocess_comment_data(date_relevant_youtube_comment_data, config.date_relevant_comment_words_csv_folder_path)
-    print('\n\n\nDONE!\n\n\n')
+    d_list = get_comment_data()
+    f_path = None
+    for i in range(len(d_list)):
+        if i == 0:
+            f_path = config.pdata_all_csv_folder_path
+            d_type = 'ALL DATA'
+        elif i == 1:
+            f_path = config.pdata_1w_csv_folder_path
+            d_type = '0 WEEK - 1 WEEK'
+        else:
+            f_path = config.pdata_2w_csv_folder_path
+            d_type = '1 WEEK - 2 WEEK'
+
+        data_len_list = data_length(d_list[i])
+
+        for dup_opt in config.pdata_type[0]:
+            print('\n\n**********************************************')
+            print('**********************************************')
+            print('**********', d_type, dup_opt.upper(), config.pdata_type[1][0].upper(), '**********')
+            print('**********************************************')
+            print('**********************************************')
+            print()
+            d_c = count(d_list[i], dup_opt)
+            save_data_file(d_c, f_path[dup_opt][config.pdata_type[1][0]], config.pdata_type[1][0])
+            d_r = ratio(d_c, data_len_list)
+            save_data_file(d_r, f_path[dup_opt][config.pdata_type[1][1]], config.pdata_type[1][1])
+            d_z = z_score(d_r)
+            save_data_file(d_z, f_path[dup_opt][config.pdata_type[1][2]], config.pdata_type[1][2])
+
+    print('\n\n\nDONE!\n')
--- utils/data_preprocessor.py
+++ utils/data_preprocessor.py
@@ -1,49 +1,87 @@
 from tqdm import tqdm
 from collections import Counter
+from config.config import Config
 
 import nltk
 import string
 import time
+import operator
+import scipy.stats as stats
+
+config = Config()
 
 
 class DataPreprocessor:
-    def tokenize(comments):
-        print('\nTOKENIZE COMMENTS\n')
-        time.sleep(0.1)
-        tokenized_comments = list()
-        for sentence in tqdm(comments):
-            tokenized_comments.extend(nltk.tokenize.word_tokenize(str(sentence)))
-        return tokenized_comments
+    def tokenize(self, comments, option):
+        print('TOKENIZING COMMENTS')
+        time.sleep(0.5)
+        result = list()
+        if option == config.pdata_type[0][0]:
+            for sentence in tqdm(comments):
+                result.extend(nltk.tokenize.word_tokenize(str(sentence)))
+        elif option == config.pdata_type[0][1]:
+            for sentence in tqdm(comments):
+                result.extend(list(dict.fromkeys(nltk.tokenize.word_tokenize(str(sentence)))))
+        else:
+            print('ERROR: Option must be either "allow duplicate" or "disallow duplicate"')
+        return result
 
-    def punctuation(words):
-        print('\nREMOVE PUNCTUATION\n')
-        time.sleep(0.1)
-        return [word for word in tqdm(words) if word not in string.punctuation]
+    def punctuation(self, words):
+        print('REMOVING PUNCTUATION')
+        time.sleep(0.5)
+        punctuation_set = set(string.punctuation)
+        return [word for word in tqdm(words) if word not in punctuation_set]
 
-    def english(words):
-        print('\nKEEP ENGLISH ONLY\n')
-        time.sleep(0.1)
-        keep_english_only_words = list()
-        for word in tqdm(words):
-            if word in nltk.corpus.words.words():
-                keep_english_only_words.append(word.lower())
-        return keep_english_only_words
+    def english(self, words):
+        print('REMOVING NON-ENGLISH WORDS')
+        time.sleep(0.5)
+        result = list()
+        english_set = set(nltk.corpus.words.words())
+        for w in tqdm(words):
+            if w in english_set:
+                result.append(w.lower())
+        return result
 
-    def stopwords(words):
-        print('\nREMOVE STOPWORDS\n')
-        time.sleep(0.1)
-        return [word for word in tqdm(words) if word not in nltk.corpus.stopwords.words('english')]
+    def stopwords(self, words):
+        print('REMOVING STOPWORDS')
+        time.sleep(0.5)
+        stopwords_set = set(nltk.corpus.stopwords.words('english'))
+        return [word for word in tqdm(words) if word not in stopwords_set]
 
-    def word_stem(words):
-        print('\nWORD STEM\n')
-        time.sleep(0.1)
+    def word_stem(self, words):
+        print('WORD STEM')
+        time.sleep(0.5)
         stemmer = nltk.stem.porter.PorterStemmer()
-        word_stem_words = list()
+        result = list()
         for word in tqdm(words):
-            word_stem_words.append(stemmer.stem(word))
-        return word_stem_words
+            result.append(stemmer.stem(word))
+        return result
 
-    def word_count(words):
-        print('\nWORD COUNT\n')
-        time.sleep(0.1)
+    def word_count(self, words):
+        print('COUNTING WORD')
+        time.sleep(0.5)
         return Counter(words)
+
+    def calculate_ratio(self, counts, comments_len, category):
+        print('CALCULATING DATA - INVERSE DATA')
+        time.sleep(0.5)
+        result = dict()
+        for i in tqdm(counts[config.status[0]][category]):
+            result[i[0]] = i[1] / comments_len[config.status[0]][category]
+        for i in tqdm(counts[config.status[1]][category]):
+            if i[0] in result:
+                result[i[0]] = result[i[0]] - (i[1] / comments_len[config.status[1]][category])
+            else:
+                result[i[0]] = -i[1]
+        return result
+
+    def calculate_z_score(self, ratios):
+        print('CALCULATING Z-SCORE')
+        time.sleep(0.5)
+        z_scores, result = list(), list()
+        for i in tqdm(ratios):
+            z_scores.append(i[1])
+        z_scores = stats.zscore(z_scores)
+        for i in tqdm(range(len(ratios))):
+            result.append([ratios[i][0], z_scores[i]])
+        return result
