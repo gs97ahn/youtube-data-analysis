5a9127863ac9c94ca75edcf3bdbb4f0ec3a74641
gs97ahn <gs97ahn@naver.com>
2022-11-25 23:10:59 +0900

    [STYLE] folder variable name change

--- web_scrape.py
+++ web_scrape.py
@@ -20,22 +20,19 @@ def web_scrape_youtube_channel_statistics():
             data[s][c].extend(d)
             raw[s][c].extend(r)
 
-    if not os.path.isdir(config.data_parent_folder_path):
-        os.mkdir(config.data_parent_folder_path)
+    for s in config.status:
+        if not os.path.isdir(config.cdata_channel_statistics_csv_folder_path[s]):
+            os.makedirs(config.cdata_channel_statistics_csv_folder_path[s])
+        if not os.path.isdir(config.cdata_channel_statistics_html_folder_path[s]):
+            os.makedirs(config.cdata_channel_statistics_html_folder_path[s])
 
     for s in config.status:
-        if not os.path.isdir(config.channel_statistics_csv_folder_path[s]):
-            os.makedirs(config.channel_statistics_csv_folder_path[s])
-        if not os.path.isdir(config.channel_statistics_html_folder_path[s]):
-            os.makedirs(config.channel_statistics_html_folder_path[s])
-
-    for s in config.status:
-        csv_folder_path = config.channel_statistics_csv_folder_path[s]
-        raw_folder_path = config.channel_statistics_html_folder_path[s]
+        csv_folder_path = config.cdata_channel_statistics_csv_folder_path[s]
+        raw_folder_path = config.cdata_channel_statistics_html_folder_path[s]
         for c in config.categories:
             print('\n\n**********', s.upper(), c.upper(), '**********')
             data_formatter.csv_saver(os.path.join(csv_folder_path, config.csv_filename[s][c]),
-                                     config.channel_statistics_header, data[s][c])
+                                     config.cdata_channel_statistics_header, data[s][c])
             data_formatter.txt_saver(os.path.join(raw_folder_path, config.raw_filename[s][c]), raw[s][c])
 
 
